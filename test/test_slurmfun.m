509a7b48f21b9c64054bacb45b8310382c79b9ab
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-09-05 11:17:11 +0200

    Fixed small bug in license checkout

--- test/test_slurmfun.m
+++ test/test_slurmfun.m
@@ -2,10 +2,12 @@
 clc
 
 
-nJobs = 300;
+nJobs = 5;
 inputArgs = num2cell(randi(20,nJobs,1)+50);
 inputArgs{end+1} = 5000000000;
 
 out = slurmfun('myfunction', inputArgs, ...
     'partition', '8GBS', ...
-    'stopOnError', false, 'deleteFiles', false);
+    'stopOnError', false, ...
+    'deleteFiles', false, ...
+    'waitForToolboxes', {'GADS_toolbox'});
